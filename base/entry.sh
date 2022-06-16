9d73dbb5dd5ba706dce672c86fd7d39334448716
agkr234 <agekara@gmail.com>
2022-06-16 17:47:22 +0900

    add: sv_downloadurl things for docker-compose

--- base/entry.sh
+++ base/entry.sh
@@ -6,6 +6,9 @@ cd "${STEAMAPPDIR}"
 # since "hostname" in command line settings will be overwritten by default server.cfg
 sed -i "s/hostname \"Counter-Strike 1.6 Server\"/hostname \"$HLDS_HOSTNAME\"/" ${STEAMAPPDIR}/${STEAMAPP}/server.cfg
 
+# configure FastDownload
+sed -i "s/sv_downloadurl \"http:\/\/127.0.0.1\/cstrike\/\"/sv_downloadurl \"$SRCDS_FASTDL\"/" ${STEAMAPPDIR}/${STEAMAPP}/server.cfg
+
 bash "${STEAMAPPDIR}/hlds_run" -game "${STEAMAPP}" \
                         -strictportbind \
                         -port "${HLDS_PORT}" \
