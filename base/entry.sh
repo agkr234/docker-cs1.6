b7ee93144da834d42ed98adb1c40fb18760b7c89
agkr234 <agekara@gmail.com>
2022-06-26 18:38:38 +0900

    fixed to use escaped env and Dockerfile ADD fix

--- base/entry.sh
+++ base/entry.sh
@@ -4,10 +4,12 @@
 cd "${STEAMAPPDIR}"
 
 # since "hostname" in command line settings will be overwritten by default server.cfg
-sed -i "s/hostname \"Counter-Strike 1.6 Server\"/hostname \"$HLDS_HOSTNAME\"/" ${STEAMAPPDIR}/${STEAMAPP}/server.cfg
+ESCAPED_HLDS_HOSTNAME="$(echo $HLDS_HOSTNAME | sed 's/[!@#$%^&*()-/]/\\&/g')"
+sed -i "s/hostname \"Counter-Strike 1.6 Server\"/hostname \"$ESCAPED_HLDS_HOSTNAME\"/" ${STEAMAPPDIR}/${STEAMAPP}/server.cfg
 
 # configure FastDownload
-sed -i "s/sv_downloadurl \"http:\/\/127.0.0.1\/cstrike\/\"/sv_downloadurl \"$FASTDL_URL\"/" ${STEAMAPPDIR}/${STEAMAPP}/server.cfg
+ESCAPED_FASTDL_URL="$(echo $FASTDL_URL | sed 's/[!@#$%^&*()-/]/\\&/g')"
+sed -i "s/sv_downloadurl \"http:\/\/127.0.0.1\/cstrike\/\"/sv_downloadurl \"$ESCAPED_FASTDL_URL\"/" ${STEAMAPPDIR}/${STEAMAPP}/server.cfg
 
 bash "${STEAMAPPDIR}/hlds_run" -game "${STEAMAPP}" \
                         -strictportbind \
